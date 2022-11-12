f30275bf0e6645a89061a1a87d8f566d9ea567f8
Ferdi <ferdi@hey.com>
2022-11-12 16:52:12 +0000

    set indentation options for filetype

--- lua/my/options.lua
+++ lua/my/options.lua
@@ -1,39 +1,53 @@
 -- :help options
 local options = {
-  backup = false,                          -- Creates a backup file
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  clipboard = "unnamedplus",               -- Allows neovim to access the system clipboard
-  completeopt = { "menuone", "noselect" }, -- Default option for popup buffers
-  conceallevel = 0,                        -- Turn of concealed characters
-  fileencoding = "utf-8",                  -- File encoding
-  hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 2,                         -- always show tabs
-  smartcase = true,                        -- smart case
---  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  swapfile = false,                        -- creates a swapfile
-  termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true,                         -- enable persistent undo
-  updatetime = 300,                        -- faster completion (4000ms default)
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = true,                   -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = false,                            -- display lines as one long line
-  scrolloff = 8,                           -- is one of my fav
+  fileencoding = "utf-8",
+  backup = false,
+  writebackup = false,
+  undofile = true,
+  clipboard = "unnamedplus",
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 0,
+  hlsearch = true,
+  ignorecase = true,
+  mouse = "a",
+  pumheight = 10,
+  showmode = false,
+  showtabline = 2,
+  smartcase = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  termguicolors = true,
+  timeoutlen = 300,
+  updatetime = 300,
+  expandtab = true,
+  cursorline = true,
+  number = true,
+  relativenumber = true,
+  signcolumn = "yes",
+  wrap = false,
+  scrolloff = 8,
   sidescrolloff = 8,
   list = true,
 }
+
+vim.au({'BufNewFile', 'BufRead'}, {
+  '*.py,*.c,*.cpp,*',
+  function ()
+    vim.opt.tabstop = 4
+    vim.opt.softtabstop = 4
+    vim.opt.shiftwidth = 4
+  end
+})
+
+vim.au({'BufNewFile', 'BufRead'}, {
+  '*.js,*.jsx,*.ts,*.tsx,*.html,*.css,*.scss,*.json',
+  function ()
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+  end
+})
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
