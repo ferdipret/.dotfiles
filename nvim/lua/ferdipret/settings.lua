d9fb20ddf00c15886246e548b307b2b1df0aeedb
Ferdi <ferdi@hey.com>
2023-02-22 14:30:54 +0000

    Remove listchars

--- nvim/lua/ferdipret/settings.lua
+++ nvim/lua/ferdipret/settings.lua
@@ -12,7 +12,7 @@ local settings = {
 	mouse = 'a',
 	pumheight = 10,
 	showmode = false,
---	showtabline = 2,
+	--	showtabline = 2,
 	smartcase = true,
 	splitbelow = true,
 	splitright = true,
@@ -39,10 +39,11 @@ vim.g.loaded_netrwPlugin = 1
 vim.diagnostic.config({
 	virtual_text = false,
 })
-vim.opt.shortmess:append('c')
-vim.opt.listchars:append('space:⋅')
-vim.opt.listchars:append('tab: →')
-vim.opt.listchars:append('eol:↴')
+
+-- vim.opt.shortmess:append('c')
+-- vim.opt.listchars:append('space:⋅')
+-- vim.opt.listchars:append('tab: →')
+-- vim.opt.listchars:append('eol:↴')
 
 for k, v in pairs(settings) do
 	vim.opt[k] = v
