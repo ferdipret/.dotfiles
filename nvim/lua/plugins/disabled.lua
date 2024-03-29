4a06708e3c97a559dca91861e869ca20d4d4220e
Ferdi Pretorius <ferdinand.pretorius@oddscheckerglobalmedia.com>
2024-03-29 15:44:39 +0000

    feat: switch to lazyvim

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  -- mini.surround is too weird to get used to. Opting to go for nvim surround
+  -- instead, which is a lot more liek tpopes vim-surround
+  { "echasnovski/mini.surround", enabled = false },
+}
